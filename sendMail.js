861d7b7f4ba97ad555520ac49cc13d256bf058c4
Ayush Bamboriya <aashumodi143@gmail.com>
2024-10-01 01:09:45 +0530

    baq with nodejs and html,css

---
+++ sendMail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER, // Sender Gmail address
+        pass: process.env.GMAIL_PASS, // App password from Gmail account
+    },
+});
+
+const sendMail = async (toEmail) => {
+    const mailOptions = {
+        from: {
+            name: 'Ayush Mail',
+            address: process.env.GMAIL_USER,
+        },
+        to: toEmail, // Use the dynamic email address from the request
+        subject: "New user from landing page",
+        text: `landing page: ${toEmail}`, // plain text body
+        html: `<b>Landing page: ${toEmail}</b>`, // html body
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Email has been sent!');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports = sendMail;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const sendMail = require('./sendMail'); // Import the sendMail function
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json()); // For parsing application/json
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use('/images', express.static('/images'));
+
+
+
+
+// Serve your HTML file on the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Update the path to your HTML file
+});
+
+
+app.post('/send-email', async (req, res) => {
+    const { email } = req.body;
+    try {
+        await sendMail(email); // Send email using the provided address
+        res.status(200).send('Email sent successfully!');
+    } catch (error) {
+        res.status(500).send('Error sending email: ' + error.message);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
